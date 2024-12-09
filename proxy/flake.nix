0072c77849d31c6610f5820f5fa5dd01e4c556b0
Lucas Maunu <evolved.cow@gmail.com>
2024-12-09 00:37:02 -0600

    getting started with nix-minecraft

--- proxy/flake.nix
+++ proxy/flake.nix
@@ -2,4 +2,13 @@
   inputs = {
     nix-minecraft.url = "github:Infinidoge/nix-minecraft";
   };
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations.mySystem = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux"; # Adjust based on your system architecture
+        modules = [
+        ./configuration.nix  # Your main system configuration
+        ];
+    };
+  };
 }
